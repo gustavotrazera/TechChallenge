e765494246986d40f0fd9d47900c033d3f3a1781
Gustavo Teller Razera <gustavotellerrazera@MacBook-Air-de-Gustavo.local>
2024-08-03 18:17:01 -0300

    Adicionado smoker e region no pre processamento de variaveis
    Adicionado MinMaxScaler para a predição

--- ChallengeInsurance.py
+++ ChallengeInsurance.py
@@ -2,8 +2,7 @@ import pandas as pd
 import missingno as msno
 import matplotlib.pyplot as plt
 import TrainningModel
-from sklearn.preprocessing import LabelEncoder
-
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 # Lendo o arquivo de dados
 dados = pd.read_csv(".venv/database/insurance.csv")
@@ -16,9 +15,11 @@ print(dados.isnull().sum())
 #pré processamento de variáveis
 encoder = LabelEncoder()
 dados['gender'] = encoder.fit_transform(dados['sex'])
+dados['smoker'] = encoder.fit_transform(dados['smoker'])
+dados['region'] = encoder.fit_transform(dados['region'])
+#dummy_dados = pd.get_dummies(dados, columns=["sex"])
+print(dados.head())
 
-dummy_dados = pd.get_dummies(dados, columns=["sex"])
-#print(dummy_dados.head())
 
 
 # Visualizando em gráfico, idade por cobranças
