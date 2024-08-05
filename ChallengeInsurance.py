99d8fb30fb7357aa7982f6f915dc686c40680f74
Gustavo Teller Razera <gustavotellerrazera@MacBook-Air-de-Gustavo.local>
2024-08-04 21:04:02 -0300

    Adicionado classe de geração de gráficos Ajustado o modelo de predição

--- ChallengeInsurance.py
+++ ChallengeInsurance.py
@@ -2,32 +2,38 @@ import pandas as pd
 import missingno as msno
 import matplotlib.pyplot as plt
 import TrainningModel
+import Graphics
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 # Lendo o arquivo de dados
 dados = pd.read_csv(".venv/database/insurance.csv")
 print(dados.head())
 
-#Verificando se temos dados nulos no arquivo de insurance
+# Pré processamento da base
+
+# Verificando se temos dados nulos no arquivo de dados
 msno.matrix(dados)
 print(dados.isnull().sum())
 
-#pré processamento de variáveis
+# Normalizando as colunas que eram strings em número
 encoder = LabelEncoder()
-dados['gender'] = encoder.fit_transform(dados['sex'])
+dados['sex'] = encoder.fit_transform(dados['sex'])
 dados['smoker'] = encoder.fit_transform(dados['smoker'])
 dados['region'] = encoder.fit_transform(dados['region'])
-#dummy_dados = pd.get_dummies(dados, columns=["sex"])
-print(dados.head())
 
+# Gerando gráficos com os dados
+Graphics.generate(dados, plt, pd)
 
+# Normalizando os dados utilizando min max scaler
+x = dados.drop(columns=["charges"])
+y = dados[["charges"]]
 
-# Visualizando em gráfico, idade por cobranças
-dados.plot.scatter(x="age", y="charges")
-plt.show()
+scaler = MinMaxScaler()
+scaler.fit(x)
+x_scaled = scaler.transform(x)
 
 # Treinamento
-TrainningModel.trainModel(dados, plt)
+TrainningModel.trainModel(x_scaled, y, plt)
 
 
 
--- TrainningModel.py
+++ TrainningModel.py
@@ -1,29 +1,17 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
+def trainModel(x_scaled, y_scaled, plt):
 
-def trainModel(dados, plt):
-
-    x = dados.drop(columns=['sex'])
-    y = dados[['charges']]
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
-
+    x_train, x_test, y_train, y_test = train_test_split(x_scaled, y_scaled, test_size=0.2, random_state=42)
     print("Total base de treino: ", len(x_train))
     print("Total base de teste: ", len(y_test))
 
-    # Normalizando os dados utilizando min max scaler
-    scaler = MinMaxScaler()
-    scaler.fit(x_train)
-    x_train_min_max_scaled = scaler.transform(x_train)
-    x_test_min_max_scaled = scaler.transform(x_test)
-
     # Usando linear Regression
     modelo_classificador = LinearRegression()
-    modelo_classificador.fit(x_train_min_max_scaled, y_train)
-    previsoes = modelo_classificador.predict(x_test_min_max_scaled)
+    modelo_classificador.fit(x_train, y_train)
+    previsoes = modelo_classificador.predict(x_test)
 
     # Avaliando o desempenho do modelo
     erro_medio_quadratico = mean_squared_error(y_test, previsoes)
@@ -33,12 +21,3 @@ def trainModel(dados, plt):
     print(f'Erro Médio Quadrático: {erro_medio_quadratico}')
     print(f'Erro Absoluto Médio: {erro_absoluto_medio}')
     print(f'R² (coeficiente de determinação): {r_quadrado}')
-
-    # Visualizando as previsões
-    plt.scatter(x_test_min_max_scaled, y_test, label='Real')
-    plt.scatter(x_test_min_max_scaled, previsoes, label='Previsto', color='red')
-    plt.xlabel('Age ')
-    plt.ylabel('Charges')
-    plt.title('Previsões do Modelo de Regressão Linear')
-    plt.legend()
-    plt.show()
